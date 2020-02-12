e31b5ee9e927dd43c6e53a906281db9001d60927
Benedek Halasi <benedek_halasi@epam.com>
2020-02-12 14:32:45 +0100

    gulpfile useref fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var uglify = require('gulp-uglify')
+var gulpIf = require('gulp-if')
+var cssnano = require('gulp-cssnano')
 
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create()
 
 gulp.task('sass', () => {
   return gulp.src('docs/scss/main.scss')
@@ -15,14 +15,6 @@ gulp.task('sass', () => {
   }))
 });
 
-gulp.task('useref', () => {
-  return gulp.src('docs/*.html')
-  .pipe(useref())
-  .pipe(gulpIf('*.js', uglify()))
-  .pipe(gulpIf('*.css', cssnano()))
-  .pipe(gulp.dest('dist'))
-});
-
 gulp.task('watch', gulp.series('sass', () => {
   browserSync.init({
     server: {
@@ -30,8 +22,8 @@ gulp.task('watch', gulp.series('sass', () => {
     },
   })
 
-  gulp.watch('docs/scss/*.scss').on('change', gulp.series('sass', 'useref'))
-  gulp.watch('docs/js/*.js').on('change', gulp.series('useref'))
+  gulp.watch('docs/scss/*.scss').on('change', gulp.series('sass'))
+  gulp.watch('docs/js/*.js').on('change', browserSync.reload)
   gulp.watch('docs/*.html').on('change', browserSync.reload) 
 }));
 
