7c492cd019ffe5ed7b73e93ed2e6157eb0ed38c8
Benedek Halasi <benedek_halasi@epam.com>
2020-02-14 19:55:54 +0100

    progress in every way: gulp, css, js, content

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
-var uglify = require('gulp-uglify')
-var gulpIf = require('gulp-if')
-var cssnano = require('gulp-cssnano')
+var ts = require('gulp-typescript')
+
+
 
 var browserSync = require('browser-sync').create()
 
 gulp.task('sass', () => {
-  return gulp.src('docs/scss/main.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('docs/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+  return gulp.src('docs/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('docs/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', gulp.series('sass', () => {
+gulp.task('ts', () => {
+  return gulp.src('docs/ts/*.ts')
+    .pipe(ts({
+      target: 'ES6',
+      outFile: 'main.js'
+    }))
+    .pipe(gulp.dest('docs/js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', gulp.series('sass', 'ts', () => {
   browserSync.init({
     server: {
       baseDir: 'docs'
@@ -23,8 +35,8 @@ gulp.task('watch', gulp.series('sass', () => {
   })
 
   gulp.watch('docs/scss/*.scss').on('change', gulp.series('sass'))
-  gulp.watch('docs/js/*.js').on('change', browserSync.reload)
-  gulp.watch('docs/*.html').on('change', browserSync.reload) 
+  gulp.watch('docs/ts/*.ts').on('change', gulp.series('ts'))
+  gulp.watch(['docs/*.html']).on('change', browserSync.reload)
 }));
 
 
